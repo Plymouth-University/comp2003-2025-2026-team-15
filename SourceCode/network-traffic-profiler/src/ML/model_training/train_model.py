73ff9ad6950c63b9c45120fa3a2726b512c93c20
soph ⊹₊♡⋆ <sophia.krasowski@students.plymouth.ac.uk>
2026-02-22 13:10:55 +0000

    reduced overfitting in ML by introducing groups, including cross validation, and excluding low permutation features; model doesn't perform well on like and subscribe, possibly due to similar traffic patterns

--- SourceCode/network-traffic-profiler/src/ML/model_training/train_model.py
+++ SourceCode/network-traffic-profiler/src/ML/model_training/train_model.py
@@ -1,44 +1,70 @@
 # train the model using the training data from /action_classification
 # from .../src/ML run python .\model_training\train_model.py
 
-# using sklearn metrics this produces an accuracy of around 80% on the test set rn
+# using sklearn metrics this produces an accuracy of around 75% on the test set rn
+# model doesn't perform well on like and subscribe
 
 import pandas as pd
 import numpy as np
 import joblib
 
 # using scikit-learn libraries so we don't have to write our code from scratch
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GroupKFold
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, accuracy_score
 from test_model import run_tests
+from sklearn.model_selection import GroupShuffleSplit, cross_val_score
 
 # load dataset created in /action_classification
 df = pd.read_csv("action_classification/master_training_data.csv")
 
-# define X and y
-# drop non-feature columns
-X = df.drop(columns=["action", "file_source"])
-# variable we want to predict
+# define X and y, features with low permutation importance are not included to reduce overfitting
+X = df[["duration", "std_iat", "avg_iat", "pk_count", "avg_inbound_size", "avg_outbound_size"]]
 y = df["action"]
+groups = df["file_source"]
 
 # encode labels
 label_encoder = LabelEncoder() # sklearn's label encoder to convert string labels to integers
 y_encoded = label_encoder.fit_transform(y)
 
-# train-test split: https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y_encoded, test_size=0.33, random_state=42, stratify=y_encoded
-)
+# train-test split with group shuffle to reduce data leakage and overfitting
+groups = df["file_source"]
+
+# use GroupShuffleSplit to create a single train/test split
+gss = GroupShuffleSplit(n_splits=1, test_size=0.33, random_state=42)
+train_idx, test_idx = next(gss.split(X, y_encoded, groups))
+
+X_train = X.iloc[train_idx]
+X_test = X.iloc[test_idx]
+y_train = y_encoded[train_idx]
+y_test = y_encoded[test_idx]
 
 # instantiate Random Forest model: https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html
 model = RandomForestClassifier(
-    n_estimators=200, # number of trees -> more trees = better performance but longer training time
-    max_depth=None,
-    random_state=42
+    n_estimators=700, # number of trees -> more trees = better performance but longer training time
+    max_depth=12, # limit depth to reduce overfitting
+    min_samples_split=15, # require more samples to split a node, reduces overfitting
+    min_samples_leaf=8, # avoid tiny leaves, reduces overfitting
+    class_weight="balanced",
+    random_state=42,
+    n_jobs=-1
 )
 
+# cross validation to reduce overfitting and bias
+gkf = GroupKFold(n_splits=5)
+
+cv_scores = cross_val_score(
+    model,
+    X,
+    y_encoded,
+    cv=gkf,
+    groups=groups,
+    scoring="accuracy",
+    n_jobs=-1
+)
+
+# train model
 model.fit(X_train, y_train)
 
 # evaluate the model by making predictions on the test set and comparing to true labels
@@ -49,9 +75,10 @@ print("Accuracy: {:.2f}%".format(accuracy_score(y_test, y_pred) * 100))
 print("\nClassification Report:\n") # for information see https://scikit-learn.org/stable/modules/generated/sklearn.metrics.classification_report.html
 print(classification_report(y_test, y_pred, target_names=label_encoder.classes_))
 
-# save the model and label encoder
-joblib.dump(model, "traffic_model.pkl")
+# save the model, label encoder and model features ensuring features are in correct order
+joblib.dump(model, "action_model.pkl")
 joblib.dump(label_encoder, "label_encoder.pkl")
+joblib.dump(X.columns.tolist(), "model_features.pkl")
 
 print("\nModel saved") # meaning .pkl files have been created and can be used now in predict.py
 
